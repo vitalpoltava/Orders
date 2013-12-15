9254f2c91c967d0a97daefcd0a3a4066ec168d11
vitalpoltava <omelkin@gmail.com>
2013-12-15 12:59:40 +0200

    less http requests

--- orders/orders.js
+++ orders/orders.js
@@ -1,3 +1,22 @@
+requirejs.config({
+    paths: {
+        jquery: './utils/jquery',
+        underscore: './utils/underscore',
+        backbone: './utils/backbone',
+        text: './utils/require_text',
+        jst: './utils/require_jst'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        }
+    }
+});
+
 define(function(require) {
     'use strict';
     var App = require('./views/mainView');
